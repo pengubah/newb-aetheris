a335691ef17857b544f3c5e524e80c75bf3bec92
pengubah <goboyy2011@gmail.com>
2026-08-19 11:56:45 +0700

    Update utils.h

--- src/newb/functions/utils.h
+++ src/newb/functions/utils.h
@@ -14,7 +14,6 @@ mat2 rmat2(float t) {
 float degToRad(float t) { return 0.0174533*t; }
 
 float luminance(vec3 x) { return dot(max(x, vec3_splat(0.0)), vec3(0.2126,0.7152,0.0722)); }
-float saturate(float x) { return clamp(x, 0.0, 1.0); }
 vec3 saturate3(vec3 x) { return clamp(x, vec3_splat(0.0), vec3_splat(1.0)); }
 float softLightCurve(float x) { x = saturate(x); return x*x*(3.0-2.0*x); }
 float remap01(float x, float a, float b) { return saturate((x-a)/max(b-a,0.0001)); }
