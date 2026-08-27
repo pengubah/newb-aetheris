1d3544ea123f32ee2abee78565726c5bdc481241
pengubah <goboyy2011@gmail.com>
2026-08-27 18:28:03 +0700

    Refactor renderCloudsRounded parameters and scaling

--- src/newb/functions/clouds.h
+++ src/newb/functions/clouds.h
@@ -59,12 +59,11 @@ float cloudDf(vec3 pos, float rain, vec2 boxiness) {
 }
 
 vec4 renderCloudsRounded(
-    vec3 vDir, vec3 vPos, float rain, float time, vec3 horizonCol, vec3 zenithCol, const float steps, const float thickness, const float thickness_rain, const float speed,
-    const float scaleX, const float scaleZ, const float density, const vec2 boxiness
+    vec3 vDir, vec3 vPos, float rain, float time, vec3 horizonCol, vec3 zenithCol, const float thickness, const float thickness_rain, const float speed,
+    const vec2 scale, const float density, const vec2 boxiness
 ) {
-  vec2 scale = vec2(scaleX,scaleZ);
   float height = 9.0*mix(thickness, thickness_rain, rain);
-  float stepsf = steps;
+  float stepsf = 5.0;
 
   // scaled ray offset
   vec3 deltaP;
