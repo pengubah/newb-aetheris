66b728529022be6f5043df95a9c3bf64113e17a5
pengubah <goboyy2011@gmail.com>
2026-09-03 20:19:43 +0700

    Add ACES Filmic tonemapping option

--- src/newb/functions/tonemap.h
+++ src/newb/functions/tonemap.h
@@ -22,6 +22,17 @@ vec3 colorCorrection(vec3 col) {
     const float e = 0.14;
     col *= 0.85;
     col = clamp((col*(a*col + b)) / (col*(c*col + d) + e), 0.0, 1.0);
+  
+    #elif NL_TONEMAP_TYPE == 5
+    // ACES Filmic
+    const float a = 2.30;
+    const float b = 0.025;
+    const float c = 2.20;
+    const float d = 0.55;
+    const float e = 0.14;
+    col *= 0.75;
+    col = clamp((col*(a*col + b)) / (col*(c*col + d) + e), 0.0, 1.0);
+  
   #elif NL_TONEMAP_TYPE == 2
     // simple reinhard tonemap
     col = col/(1.0+col);
