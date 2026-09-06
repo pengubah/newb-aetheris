730611901ca89eb113d96ca9f21c688f687e6ad5
pengubah <goboyy2011@gmail.com>
2026-09-07 00:17:18 +0700

    Adjust water and lava effect parameters

--- src/newb/config.h
+++ src/newb/config.h
@@ -116,8 +116,8 @@
 
 /* Water */
 #define NL_WATER_TRANSPARENCY 0.9 // 0.0 transparent ~ 1.0 normal
-#define NL_WATER_BUMP 0.08         // 0.001 plain ~ 0.2 bumpy water
-#define NL_WATER_WAVE_SPEED  0.65  // 0.2 calm ~ 2.0 turbulent
+#define NL_WATER_BUMP 0.09         // 0.001 plain ~ 0.2 bumpy water
+#define NL_WATER_WAVE_SPEED  0.7  // 0.2 calm ~ 2.0 turbulent
 #define NL_WATER_TEX_OPACITY 0.0  // 0.0 plain water ~ 1.0 vanilla water texture
 #define NL_WATER_WAVE             // [toggle] wave effect
 //#define NL_WATER_REFL_MASK      // [toggle] fake water reflection mask
@@ -212,8 +212,8 @@
 
 /* Lava effects */
 #define NL_LAVA_NOISE            // [toggle] darken lava in certain regions
-#define NL_LAVA_NOISE_BUMP 0.2   // [toggle] 0.1 subtle ~ 0.8 massive waves
-#define NL_LAVA_NOISE_SPEED 0.2  // 0.0 still ~ 0.8 fast
+#define NL_LAVA_NOISE_BUMP 0.16   // [toggle] 0.1 subtle ~ 0.8 massive waves
+#define NL_LAVA_NOISE_SPEED 0.18  // 0.0 still ~ 0.8 fast
 
 /*
   NEWB SHADER SUBPACK CONFIG
