ccd99bd2e1d5dead528a49db6b11c11e7ee926df
pengubah <goboyy2011@gmail.com>
2026-09-07 08:55:30 +0700

    Update lighting.h

--- src/newb/functions/lighting.h
+++ src/newb/functions/lighting.h
@@ -9,11 +9,9 @@
 
 vec3 sunLightTint(float dayFactor, float rain) {
   float nightFactor = step(dayFactor, 0.0);
-  float dawnFactor = 1.0-dayFactor*dayFactor;
-  dawnFactor *= dawnFactor*dawnFactor;
-  dawnFactor *= mix(1.0, dawnFactor*dawnFactor, nightFactor);
+  float dawnFactor = nlDawnShape(dayFactor);
   vec3 tint = mix(NL_NOON_SUNLIGHT_COL, NL_NIGHT_MOONLIGHT_COL, nightFactor);
-  tint = mix(tint, NL_DAWN_SUNLIGHT_COL, dawnFactor*0.6);
+  tint = mix(tint, NL_DAWN_SUNLIGHT_COL, dawnFactor);
   tint = mix(tint, vec3_splat(dot(tint, vec3_splat(0.33))), rain);
   return tint;
 }
