101aff8af0305fa762b5c17eab229511d3d8d86d
pengubah <goboyy2011@gmail.com>
2026-09-08 14:37:11 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -37,7 +37,7 @@
 #define NL_TINT_HIGH vec3(1.0,1.0,1.0) // color tint for light tone
 
 /* Lighting */
-#define NL_SUNLIGHT_INTENSITY   2.85  // 1.0 weak ~ 5.0 bright
+#define NL_SUNLIGHT_INTENSITY   2.9  // 1.0 weak ~ 5.0 bright
 #define NL_TORCHLIGHT_INTENSITY 1.2  // 0.5 weak ~ 3.0 bright
 #define NL_SHADOW_INTENSITY     1.0  // 0.0 no shadow ~ 1.0 strong shadow
 #define NL_MIN_LIGHTING_BOOST   0.82 // 1.0 minimal lighting boost for dark areas ~ 3.0 brighter dark areas
