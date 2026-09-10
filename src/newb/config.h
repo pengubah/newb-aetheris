4183a918cead702472cb29a113ac95c7e26863ae
pengubah <goboyy2011@gmail.com>
2026-09-10 09:52:48 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -75,9 +75,9 @@
 #define NL_DAY_HORIZON_COL   vec3(0.18,0.80,1.50)
 #define NL_DAY_EDGE_COL      vec3(0.525,1.05,1.50)
 
-#define NL_DAWN_ZENITH_COL   vec3(0.10,0.11,0.44)
-#define NL_DAWN_HORIZON_COL  vec3(1.00,0.45,0.00)*1.5
-#define NL_DAWN_EDGE_COL     vec3(1.00,0.45,0.00)*1.5
+#define NL_DAWN_ZENITH_COL   vec3(0.32,0.27,0.40)
+#define NL_DAWN_HORIZON_COL  vec3(1.00,0.68,0.12)*1.5
+#define NL_DAWN_EDGE_COL     vec3(0.72,0.55,0.18)*1.5
 
 #define NL_NIGHT_ZENITH_COL   vec3(0.004,0.01,0.03)
 #define NL_NIGHT_HORIZON_COL  vec3(0.012,0.032,0.075)
