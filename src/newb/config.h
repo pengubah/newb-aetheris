a8f43d85f174bf5a10738ff5251407f2799fe2cc
pengubah <goboyy2011@gmail.com>
2026-09-13 04:51:50 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -51,7 +51,7 @@
 /* Sun/moon light color */
 #define NL_DAWN_SUNLIGHT_COL   vec3(1.00,0.98,0.98)
 #define NL_NOON_SUNLIGHT_COL   vec3(0.75,0.8,1.00)
-#define NL_NIGHT_MOONLIGHT_COL vec3(0.01,0.03,0.2)
+#define NL_NIGHT_MOONLIGHT_COL vec3(0.001,0.001,0.002)
 
 /* Torch colors */
 #define NL_OVERWORLD_TORCH_COL   vec3(1.00,0.80,0.60)
